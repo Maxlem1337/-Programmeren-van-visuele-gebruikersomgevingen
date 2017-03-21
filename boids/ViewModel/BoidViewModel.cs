ce7056b191a35fb081702cbb9ccde5e9ae706e13
Maxim Lemmens <maxim-lemmens@hotmail.com>
2017-03-21 18:26:15 +0100

    things

---
+++ boids/ViewModel/BoidViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Mathematics;
+using Model;
+using Model.Species;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    class BoidViewModel
+    {
+        public Boid Boid { get; private set; }
+
+        public BoidViewModel(World world, Vector2D position, BoidSpecies species)
+        {
+            this.Boid = new Boid(world, position, species);
+        }
+
+    }
+
+
+
+}
---
+++ boids/ViewModel/WorldViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Mathematics;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class WorldViewModel
+    {
+
+        public WorldViewModel()
+        {
+            this.Simulation = new Simulation();
+            this.Simulation.Species[0].CreateBoid(new Vector2D(50, 50));
+            this.Simulation.Species[1].CreateBoid(new Vector2D(150, 150));
+        }
+
+        public Simulation Simulation { get; set; }
+        
+
+    }
+}
